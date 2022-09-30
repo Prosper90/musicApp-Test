d9d2c7feb4df0932af41a103540fde61a4e4a08f
Prosper <nkonyeprospero@gmail.com>
2022-09-30 19:37:51 +0100

    making slight touches again

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css';
 import Layout from 'components/layout/Layout';
+import createEmotionCache from '../src/createEmotionCache';
+import { CacheProvider } from '@emotion/react';
+
 
 function MyApp({ Component, pageProps }) {
   return(
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <CacheProvider value={emotionCache}>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CacheProvider>
   )
 }
 
